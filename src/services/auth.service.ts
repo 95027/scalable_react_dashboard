aafe6d2ce683e8648334cac9ffbe46774d082780
sai kumar <kumarchembeti26@gmail.com>
2026-08-02 22:58:41 +0530

    feat: setup redux toolkit

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -9,8 +9,18 @@ const login = (data: LoginRequest) => {
   return api.post("/auth/login", data);
 };
 
+const getAuthUser = () => {
+  return api.get("/user/me");
+};
+
+const logout = () => {
+  return api.post("/auth/logout");
+};
+
 const authService = {
   login,
+  getAuthUser,
+  logout,
 };
 
 export default authService;
